92a67823e061261d395a18630b8ce8ae28031d98
abel2606 <abel.sanchez.guerrero@gmail.com>
2024-02-14 08:45:28 -0700

    Agregar Cuenta a base de datos

--- BancoPersistencia/src/main/java/org/itson/bdavanzadas/bancopersistencia/daos/CuentaDAO.java
+++ BancoPersistencia/src/main/java/org/itson/bdavanzadas/bancopersistencia/daos/CuentaDAO.java
@@ -11,6 +11,8 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import org.itson.bdavanzadas.bancodominio.Cuenta;
+import org.itson.bdavanzadas.bancopersistencia.conexion.IConexion;
 import org.itson.bdavanzadas.bancopersistencia.dtos.CuentaDTO;
 import org.itson.bdavanzadas.bancopersistencia.excepciones.PersistenciaException;
 
@@ -22,37 +24,52 @@ public class CuentaDAO implements ICuentaDAO{
 
     final IConexion conexionBD;
     static final Logger logger = Logger.getLogger(CuentaDAO.class.getName());
+
+    /**
+     * Constructor de la cuentaDAO
+     * @param conexionBD establece la conexion a la base de datos
+     */
+    public CuentaDAO(IConexion conexionBD) {
+        this.conexionBD = conexionBD;
+    }
+    
     
     /**
      * Agrega una cuenta a la base de datos
-     * @param cuentaAgregar
-     * @return
+     * @param cuentaAgregar objeto de la cuenta a agregar
+     * @return regresa la cuenta agregada
      * @throws PersistenciaException
      */
     @Override
     public Cuenta agregar(CuentaDTO cuentaAgregar) throws PersistenciaException {
         String sentenciaSQL = """
-                             INSERT INTO cuentas(numero,saldo,alias,fechaApertura,activa)
-                             VALUES (?,?,?);
+                             INSERT INTO cuentas(numero, saldo, alias, fechaApertura, activa)
+                             VALUES (?, ?, ?, ?, ?);
                              """;
-        try (
-                Connection conexion = this.conexionBD.obtenerConexion(); PreparedStatement comando = conexion.prepareStatement(sentenciaSQL, Statement.RETURN_GENERATED_KEYS);) {
+        try (Connection conexion = this.conexionBD.obtenerConexion();
+             PreparedStatement comando = conexion.prepareStatement(sentenciaSQL, Statement.RETURN_GENERATED_KEYS)) {
 
+            int esActiva = 0;
+            if(cuentaAgregar.isActiva()){
+                esActiva = 1;
+            }
+            
             comando.setString(1, cuentaAgregar.getNumero());
-            comando.setString(2, Float.toString(cuentaAgregar.getSaldo()));
+            comando.setFloat(2, cuentaAgregar.getSaldo());
             comando.setString(3, cuentaAgregar.getAlias());
-            comando.setString(4, cuentaAgregar.getAlias());
-            comando.setString(5, cuentaAgregar.getFechaApertura());
-            comando.setString(6, String.valueOf(cuentaAgregar.isActiva()));
+            comando.setString(4, cuentaAgregar.getFechaApertura().toString());
+            comando.setString(5, String.valueOf(esActiva));
+
             int numRegistrosInsertados = comando.executeUpdate();
             logger.log(Level.INFO, "Se agregaron {0} cuentas", numRegistrosInsertados);
 
             ResultSet idsGenerados = comando.getGeneratedKeys();
-            idsGenerados.next();
-            Cuenta socio = new Cuenta(
-                    idsGenerados.getLong(1), 
-            );
-            return socio;
+            if (idsGenerados.next()) {
+                long idGenerado = idsGenerados.getLong(1);
+                return new Cuenta(cuentaAgregar.getNumero(), cuentaAgregar.getAlias(), cuentaAgregar.getSaldo(), cuentaAgregar.getFechaApertura(), cuentaAgregar.isActiva());
+            } else {
+                throw new PersistenciaException("No se pudo obtener el ID generado para la cuenta agregada");
+            }
         } catch (SQLException ex) {
             logger.log(Level.SEVERE, "No se pudo guardar la cuenta", ex);
             throw new PersistenciaException("No se pudo guardar la cuenta");
