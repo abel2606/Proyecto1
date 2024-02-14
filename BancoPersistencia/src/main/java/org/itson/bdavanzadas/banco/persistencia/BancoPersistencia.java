4c12f7434c0124ff53cdda85b231e0426be98864
abel2606 <abel.sanchez.guerrero@gmail.com>
2024-02-13 20:59:10 -0700

    versionPrueba

--- BancoPersistencia/src/main/java/org/itson/bdavanzadas/banco/persistencia/BancoPersistencia.java
+++ BancoPersistencia/src/main/java/org/itson/bdavanzadas/banco/persistencia/BancoPersistencia.java
@@ -6,6 +6,6 @@ public class BancoPersistencia {
         System.out.println("Hello World!");
         
         
-        System.out.println("Hola richi :)");
+        System.out.println("Hola richi :)   ");
     }
 }
