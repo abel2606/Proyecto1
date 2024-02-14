ce981ba83514fbafec8921805fc608ea44f6a29a
imnotrichi <116768255+imnotrichi@users.noreply.github.com>
2024-02-13 20:50:55 -0700

    Primer Cambio Richi

--- BancoPersistencia/src/main/java/org/itson/bdavanzadas/banco/persistencia/BancoPersistencia.java
+++ BancoPersistencia/src/main/java/org/itson/bdavanzadas/banco/persistencia/BancoPersistencia.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package org.itson.bdavanzadas.banco.persistencia;
 
-/**
- *
- * @author Abe
- */
 public class BancoPersistencia {
 
     public static void main(String[] args) {
