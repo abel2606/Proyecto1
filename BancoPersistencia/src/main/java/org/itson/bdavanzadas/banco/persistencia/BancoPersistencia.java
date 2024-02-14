11e5a212eb7466f213fc1b1fc580633172a7a7e1
imnotrichi <116768255+imnotrichi@users.noreply.github.com>
2024-02-13 21:00:34 -0700

    Otro Cambio del Richi

--- BancoPersistencia/src/main/java/org/itson/bdavanzadas/banco/persistencia/BancoPersistencia.java
+++ BancoPersistencia/src/main/java/org/itson/bdavanzadas/banco/persistencia/BancoPersistencia.java
@@ -5,7 +5,7 @@ public class BancoPersistencia {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         
-        
+        System.out.println("Hola Abelardo");
         System.out.println("Hola richi :)");
     }
 }
