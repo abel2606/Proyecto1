a5dfe29c73a2a8a3225b1ff795ea45f9a116e71b
imnotrichi <116768255+imnotrichi@users.noreply.github.com>
2024-02-14 23:00:02 -0700

    Merge origin/master
    Conflicts:
    	BancoPersistencia/src/main/java/org/itson/bdavanzadas/banco/persistencia/BancoPersistencia.java

--- Banco/src/main/java/org/itson/bdavanzadas/banco/Banco.java
+++ Banco/src/main/java/org/itson/bdavanzadas/banco/Banco.java
@@ -1,11 +1,17 @@
 package org.itson.bdavanzadas.banco;
 
+import java.util.logging.Level;
 import org.itson.bdavanzadas.bancopersistencia.conexion.Conexion;
 import org.itson.bdavanzadas.bancopersistencia.conexion.IConexion;
 import java.util.logging.Logger;
 import org.itson.bdavanzadas.banco.interfaces.PantallaCuentas;
+import org.itson.bdavanzadas.bancodominio.Cliente;
+import org.itson.bdavanzadas.bancodominio.Fecha;
 import org.itson.bdavanzadas.bancopersistencia.daos.CuentasDAO;
+import org.itson.bdavanzadas.bancopersistencia.daos.IClientesDAO;
 import org.itson.bdavanzadas.bancopersistencia.daos.ICuentasDAO;
+import org.itson.bdavanzadas.bancopersistencia.dtos.ClienteNuevoDTO;
+import org.itson.bdavanzadas.bancopersistencia.excepciones.PersistenciaException;
 
 public class Banco {
 
@@ -18,8 +24,23 @@ public class Banco {
 
         IConexion conexion = new Conexion(cadenaConexion, usuario, contrasenia);
         ICuentasDAO cuentaDAO = new CuentasDAO(conexion);
+        IClientesDAO clientesDAO = new ClientesDAO(conexion);
         PantallaCuentas pantallaCuentas = new PantallaCuentas(conexion);
         pantallaCuentas.setVisible(true);
+        
+        ClienteNuevoDTO clienteNuevo = new ClienteNuevoDTO();
+        clienteNuevo.setNombre("Ricardo Alán");
+        clienteNuevo.setApellidoPaterno("Gutiérrez");
+        clienteNuevo.setApellidoMaterno("Garcés");
+        clienteNuevo.setFechaNacimiento(new Fecha(21, 03, 2004));
+        clienteNuevo.setUsuario("imnotrichi");
+        clienteNuevo.setContrasena("password");
+        
+        try {
+            Cliente cliente = clientesDAO
+        } catch (PersistenciaException e) {
+            logger.log(Level.SEVERE, null, e);
+        }
 
 //        CuentaNuevaDTO cuentaNueva = new CuentaNuevaDTO();
 //        cuentaNueva.setSaldo(0);
