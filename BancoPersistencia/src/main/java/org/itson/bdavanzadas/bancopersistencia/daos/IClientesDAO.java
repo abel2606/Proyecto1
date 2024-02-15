22ff5807cbe0e4cebe99095f49d702064b7a82a0
imnotrichi <116768255+imnotrichi@users.noreply.github.com>
2024-02-15 01:57:38 -0700

    Merge origin/master
    Conflicts:
    	BancoPersistencia/src/main/java/org/itson/bdavanzadas/banco/persistencia/BancoPersistencia.java

--- BancoPersistencia/src/main/java/org/itson/bdavanzadas/bancopersistencia/daos/IClientesDAO.java
+++ BancoPersistencia/src/main/java/org/itson/bdavanzadas/bancopersistencia/daos/IClientesDAO.java
@@ -1,11 +1,14 @@
 package org.itson.bdavanzadas.bancopersistencia.daos;
 
+import java.util.List;
 import org.itson.bdavanzadas.bancodominio.Cliente;
 import org.itson.bdavanzadas.bancopersistencia.dtos.ClienteNuevoDTO;
 import org.itson.bdavanzadas.bancopersistencia.excepciones.PersistenciaException;
 
 public interface IClientesDAO {
 
+    List<Cliente> consultar() throws PersistenciaException;
+    
     /**
      * Permite agregar un cliente nuevo a la base de datos.
      *
