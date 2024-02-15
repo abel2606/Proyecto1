36bb020f4d3abf04bb78d340808b6c120db2970c
abel2606 <abel.sanchez.guerrero@gmail.com>
2024-02-14 23:01:01 -0700

    Merge origin/master into abelBranch
    Conflicts:
    	BancoPersistencia/src/main/java/org/itson/bdavanzadas/bancopersistencia/daos/CuentaDAO.java

--- BancoPersistencia/src/main/java/org/itson/bdavanzadas/bancopersistencia/daos/IClientesDAO.java
+++ BancoPersistencia/src/main/java/org/itson/bdavanzadas/bancopersistencia/daos/IClientesDAO.java
@@ -1,5 +1,9 @@
 package org.itson.bdavanzadas.bancopersistencia.daos;
 
+import org.itson.bdavanzadas.bancodominio.Cliente;
+import org.itson.bdavanzadas.bancopersistencia.dtos.ClienteNuevoDTO;
+import org.itson.bdavanzadas.bancopersistencia.excepciones.PersistenciaException;
+
 public interface IClientesDAO {
-    
+    Cliente agregar(ClienteNuevoDTO clienteNuevo) throws PersistenciaException;
 }
