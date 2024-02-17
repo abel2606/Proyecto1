546084225e89ff9fd25676ba17592d392ee5db29
abel2606 <abel.sanchez.guerrero@gmail.com>
2024-02-16 17:41:27 -0700

    Modificaciones de cliente get y set

--- BancoDominio/src/main/java/org/itson/bdavanzadas/bancodominio/Cliente.java
+++ BancoDominio/src/main/java/org/itson/bdavanzadas/bancodominio/Cliente.java
@@ -213,6 +213,47 @@ private Long id;
         this.contrasena = contrasena;
     }
 
+    public String getCalle() {
+        return calle;
+    }
+
+    public void setCalle(String calle) {
+        this.calle = calle;
+    }
+
+    public String getColonia() {
+        return colonia;
+    }
+
+    public void setColonia(String colonia) {
+        this.colonia = colonia;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getCodigoPostal() {
+        return codigoPostal;
+    }
+
+    public void setCodigoPostal(String codigoPostal) {
+        this.codigoPostal = codigoPostal;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    
     /**
      * Permite establecer un código único al cliente basándose en el id.
      *
