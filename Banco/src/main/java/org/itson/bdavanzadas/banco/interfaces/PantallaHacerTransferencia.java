8bfdb95dd3b667cd22826636d5544732ece8685e
imnotrichi <116768255+imnotrichi@users.noreply.github.com>
2024-02-19 09:09:22 -0700

    Cambios en Banco

--- Banco/src/main/java/org/itson/bdavanzadas/banco/interfaces/PantallaHacerTransferencia.java
+++ Banco/src/main/java/org/itson/bdavanzadas/banco/interfaces/PantallaHacerTransferencia.java
@@ -25,6 +25,10 @@ public class PantallaHacerTransferencia extends javax.swing.JDialog {
         this.conexion = conexion;
         this.cuenta = cuenta;
         cuentasDAO = new CuentasDAO(conexion);
+        
+        txtAliasCuenta.setText(cuenta.getAlias());
+        txtNumeroCuenta.setText(cuenta.getNumero().toString());
+        txtSaldo.setText(cuenta.getSaldo().toString());
     }
 
     /**
@@ -57,6 +61,20 @@ public class PantallaHacerTransferencia extends javax.swing.JDialog {
         lblTitulo = new javax.swing.JLabel();
         lblPokebolaIzq = new javax.swing.JLabel();
         lblPokebolaDer = new javax.swing.JLabel();
+        pswContrasena = new javax.swing.JPasswordField();
+        txtFolio = new javax.swing.JTextField();
+        lblFolio = new javax.swing.JLabel();
+        lblContrasena = new javax.swing.JLabel();
+        txtAliasCuenta = new javax.swing.JTextField();
+        lblDatosCuenta = new javax.swing.JLabel();
+        txtSaldo = new javax.swing.JTextField();
+        lblSaldo = new javax.swing.JLabel();
+        lblCuenta = new javax.swing.JLabel();
+        lblNumeroCuenta = new javax.swing.JLabel();
+        txtNumeroCuenta = new javax.swing.JTextField();
+        lblDatosTransferencia = new javax.swing.JLabel();
+        btnCancelar = new javax.swing.JButton();
+        btnTransferir = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         setResizable(false);
@@ -82,9 +100,9 @@ public class PantallaHacerTransferencia extends javax.swing.JDialog {
             .addGroup(jPanel2Layout.createSequentialGroup()
                 .addGap(19, 19, 19)
                 .addComponent(lblPokebolaIzq)
-                .addGap(174, 174, 174)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(lblTitulo)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 175, Short.MAX_VALUE)
+                .addGap(175, 175, 175)
                 .addComponent(lblPokebolaDer)
                 .addGap(19, 19, 19))
         );
@@ -93,23 +111,168 @@ public class PantallaHacerTransferencia extends javax.swing.JDialog {
             .addGroup(jPanel2Layout.createSequentialGroup()
                 .addGap(8, 8, 8)
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(lblPokebolaIzq)
                     .addComponent(lblTitulo)
+                    .addComponent(lblPokebolaIzq)
                     .addComponent(lblPokebolaDer))
                 .addContainerGap(13, Short.MAX_VALUE))
         );
+
+        pswContrasena.setFont(new java.awt.Font("Arial", 1, 30)); // NOI18N
+        pswContrasena.setForeground(new java.awt.Color(99, 134, 107));
+        pswContrasena.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(0, 255, 117), 2, true));
+
+        txtFolio.setFont(new java.awt.Font("Arial", 1, 30)); // NOI18N
+        txtFolio.setForeground(new java.awt.Color(99, 134, 107));
+        txtFolio.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(0, 255, 117), 2, true));
+
+        lblFolio.setBackground(new java.awt.Color(255, 255, 255));
+        lblFolio.setFont(new java.awt.Font("Arial", 1, 30)); // NOI18N
+        lblFolio.setForeground(new java.awt.Color(41, 92, 52));
+        lblFolio.setText("Número de la Cuenta Destino");
+
+        lblContrasena.setBackground(new java.awt.Color(255, 255, 255));
+        lblContrasena.setFont(new java.awt.Font("Arial", 1, 30)); // NOI18N
+        lblContrasena.setForeground(new java.awt.Color(41, 92, 52));
+        lblContrasena.setText("Monto de la Transferencia");
+
+        txtAliasCuenta.setEditable(false);
+        txtAliasCuenta.setFont(new java.awt.Font("Arial", 1, 30)); // NOI18N
+        txtAliasCuenta.setForeground(new java.awt.Color(99, 134, 107));
+        txtAliasCuenta.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(0, 255, 117), 2, true));
+
+        lblDatosCuenta.setBackground(new java.awt.Color(255, 255, 255));
+        lblDatosCuenta.setFont(new java.awt.Font("Arial", 1, 30)); // NOI18N
+        lblDatosCuenta.setForeground(new java.awt.Color(41, 92, 52));
+        lblDatosCuenta.setText("DATOS DE LA CUENTA");
+
+        txtSaldo.setEditable(false);
+        txtSaldo.setFont(new java.awt.Font("Arial", 1, 30)); // NOI18N
+        txtSaldo.setForeground(new java.awt.Color(99, 134, 107));
+        txtSaldo.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(0, 255, 117), 2, true));
+
+        lblSaldo.setBackground(new java.awt.Color(255, 255, 255));
+        lblSaldo.setFont(new java.awt.Font("Arial", 1, 30)); // NOI18N
+        lblSaldo.setForeground(new java.awt.Color(41, 92, 52));
+        lblSaldo.setText("Saldo");
+
+        lblCuenta.setBackground(new java.awt.Color(255, 255, 255));
+        lblCuenta.setFont(new java.awt.Font("Arial", 1, 30)); // NOI18N
+        lblCuenta.setForeground(new java.awt.Color(41, 92, 52));
+        lblCuenta.setText("Alias");
+
+        lblNumeroCuenta.setBackground(new java.awt.Color(255, 255, 255));
+        lblNumeroCuenta.setFont(new java.awt.Font("Arial", 1, 30)); // NOI18N
+        lblNumeroCuenta.setForeground(new java.awt.Color(41, 92, 52));
+        lblNumeroCuenta.setText("Número");
+
+        txtNumeroCuenta.setEditable(false);
+        txtNumeroCuenta.setFont(new java.awt.Font("Arial", 1, 30)); // NOI18N
+        txtNumeroCuenta.setForeground(new java.awt.Color(99, 134, 107));
+        txtNumeroCuenta.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(0, 255, 117), 2, true));
+
+        lblDatosTransferencia.setBackground(new java.awt.Color(255, 255, 255));
+        lblDatosTransferencia.setFont(new java.awt.Font("Arial", 1, 30)); // NOI18N
+        lblDatosTransferencia.setForeground(new java.awt.Color(41, 92, 52));
+        lblDatosTransferencia.setText("DATOS DE LA TRANSFERENCIA");
+
+        btnCancelar.setBackground(new java.awt.Color(0, 255, 117));
+        btnCancelar.setFont(new java.awt.Font("Arial", 1, 29)); // NOI18N
+        btnCancelar.setForeground(new java.awt.Color(255, 255, 255));
+        btnCancelar.setText("CANCELAR");
+        btnCancelar.setPreferredSize(new java.awt.Dimension(179, 47));
+        btnCancelar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnCancelarActionPerformed(evt);
+            }
+        });
+
+        btnTransferir.setBackground(new java.awt.Color(0, 255, 117));
+        btnTransferir.setFont(new java.awt.Font("Arial", 1, 29)); // NOI18N
+        btnTransferir.setForeground(new java.awt.Color(255, 255, 255));
+        btnTransferir.setText("TRANSFERIR");
+        btnTransferir.setPreferredSize(new java.awt.Dimension(200, 47));
+        btnTransferir.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnTransferirActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, 1100, Short.MAX_VALUE)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(120, 120, 120)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(lblFolio)
+                            .addComponent(lblContrasena))
+                        .addGap(18, 18, 18)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(pswContrasena)
+                            .addComponent(txtFolio)))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(lblCuenta)
+                        .addGap(18, 18, 18)
+                        .addComponent(txtAliasCuenta, javax.swing.GroupLayout.PREFERRED_SIZE, 300, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 41, Short.MAX_VALUE)
+                        .addComponent(lblNumeroCuenta)
+                        .addGap(18, 18, 18)
+                        .addComponent(txtNumeroCuenta, javax.swing.GroupLayout.PREFERRED_SIZE, 300, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(btnCancelar, javax.swing.GroupLayout.PREFERRED_SIZE, 222, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(btnTransferir, javax.swing.GroupLayout.PREFERRED_SIZE, 222, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addGap(120, 120, 120))
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(350, 350, 350)
+                        .addComponent(lblSaldo)
+                        .addGap(18, 18, 18)
+                        .addComponent(txtSaldo, javax.swing.GroupLayout.PREFERRED_SIZE, 300, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(317, 317, 317)
+                        .addComponent(lblDatosTransferencia)))
+                .addGap(0, 0, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(lblDatosCuenta)
+                .addGap(381, 381, 381))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(516, Short.MAX_VALUE))
+                .addGap(33, 33, 33)
+                .addComponent(lblDatosCuenta)
+                .addGap(18, 18, 18)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(lblCuenta)
+                    .addComponent(txtAliasCuenta, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(lblNumeroCuenta)
+                    .addComponent(txtNumeroCuenta, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(31, 31, 31)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(lblSaldo)
+                    .addComponent(txtSaldo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(44, 44, 44)
+                .addComponent(lblDatosTransferencia)
+                .addGap(18, 18, 18)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(lblFolio)
+                    .addComponent(txtFolio, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(31, 31, 31)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(pswContrasena, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(lblContrasena))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 30, Short.MAX_VALUE)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btnCancelar, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btnTransferir, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(33, 33, 33))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -126,12 +289,34 @@ public class PantallaHacerTransferencia extends javax.swing.JDialog {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void btnCancelarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCancelarActionPerformed
+        dispose();
+    }//GEN-LAST:event_btnCancelarActionPerformed
+
+    private void btnTransferirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnTransferirActionPerformed
+        
+    }//GEN-LAST:event_btnTransferirActionPerformed
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnCancelar;
+    private javax.swing.JButton btnTransferir;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
+    private javax.swing.JLabel lblContrasena;
+    private javax.swing.JLabel lblCuenta;
+    private javax.swing.JLabel lblDatosCuenta;
+    private javax.swing.JLabel lblDatosTransferencia;
+    private javax.swing.JLabel lblFolio;
+    private javax.swing.JLabel lblNumeroCuenta;
     private javax.swing.JLabel lblPokebolaDer;
     private javax.swing.JLabel lblPokebolaIzq;
+    private javax.swing.JLabel lblSaldo;
     private javax.swing.JLabel lblTitulo;
+    private javax.swing.JPasswordField pswContrasena;
+    private javax.swing.JTextField txtAliasCuenta;
+    private javax.swing.JTextField txtFolio;
+    private javax.swing.JTextField txtNumeroCuenta;
+    private javax.swing.JTextField txtSaldo;
     // End of variables declaration//GEN-END:variables
     private IConexion conexion;
     private ICuentasDAO cuentasDAO;
